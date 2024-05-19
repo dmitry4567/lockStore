c01d0ba55329e21be703248ee08099004dacc719
dmitry4567 <dimon2003pp@gmail.com>
2024-05-19 12:13:53 +0300

    fix naming

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -6,12 +6,12 @@ export const getPostgresConfig = async (
 ): Promise<TypeOrmModuleOptions> => {
   return {
     type: 'postgres',
-    host: configService.get('POSTGRES_HOST'),
-    port: parseInt(configService.get('POSTGRES_PORT')),
-    username: configService.get('POSTGRES_USERNAME'),
-    password: configService.get('POSTGRES_PASSWORD'),
-    database: configService.get('POSTGRES_DB'),
-    synchronize: configService.get('POSTGRES_SYNCHRONIZE') === 'true',
+    host: configService.get('DATABASE_HOST'),
+    port: parseInt(configService.get('DATABASE_PORT')),
+    username: configService.get('DATABASE_USERNAME'),
+    password: configService.get('DATABASE_PASSWORD'),
+    database: configService.get('DATABASE_NAME'),
+    synchronize: configService.get('DATABASE_SYNCHRONIZE') === 'true',
     autoLoadEntities: true,
   };
 };
